0508ba690913ed5b8a6ef5797c8ca505187d19bf
Kawish-Raj <rajkawish@gmail.com>
2026-08-18 12:31:11 +0530

    added patient table to it's own div

--- pages/visits.js
+++ pages/visits.js
@@ -5,10 +5,14 @@ const contentContainer = document.querySelector("#app-content");
 const PATIENT_TABLE_HEADINGS = ["Case No","First Name","Last Name","Mobile Number","Age","Husband/Father"];
 const PATIENT_TABLE_KEYS = ["case_no","first_name","last_name","mobile_number","age","address","husband_father"];
 
+const patientTableContainer = document.createElement("div");
+patientTableContainer.id = "patient-visit-table";
+
 let cachedData = null;
 
 export async function renderVisits(){
-    // contentContainer.innerHTML = `<div id="patient-visit-table"></div>`
+    contentContainer.replaceChildren();
+    contentContainer.append(patientTableContainer);
     await displayTodayPatient();
     
 };
@@ -16,17 +20,17 @@ export async function renderVisits(){
 async function displayTodayPatient(){
     
     if (cachedData == null) {
-        contentContainer.innerHTML = "<h2>Loding...</h2>";
+        patientTableContainer.innerHTML = "<h2>Loding...</h2>";
         const { data: freshData, error } = await supabase.from('patients').select();
 
         if (error) {
             console.log(error);
-            contentContainer.innerHTML = "<p>Error loading data.</p>";
+            patientTableContainer.innerHTML = "<p>Error loading data.</p>";
             return;
         }
 
         if (!freshData || freshData.length === 0) {
-            contentContainer.innerHTML = "<p>No patients found.</p>";
+            patientTableContainer.innerHTML = "<p>No patients found.</p>";
             return;
         }
 
@@ -35,8 +39,6 @@ async function displayTodayPatient(){
     
     
 
-    console.log(cachedData);
-    // Use backticks `` and target a specific column property (e.g., .name)
-    contentContainer.innerHTML = tableComp(cachedData,PATIENT_TABLE_HEADINGS,PATIENT_TABLE_KEYS);
+    patientTableContainer.innerHTML = tableComp(cachedData,PATIENT_TABLE_HEADINGS,PATIENT_TABLE_KEYS);
 }
 
